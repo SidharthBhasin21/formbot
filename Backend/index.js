974a1d27308b098ecc0765200b52a5bff497bb00
Sidharth Bhasin <sid21bhasin@gmail.com>
2025-01-04 09:33:30 +0530

    Update CORS origin to allow requests from production URL

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const dashboardRouter = require('./routes/dashboardRouter')
 
 db()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://formbot-zk2y.vercel.app/",
     credentials: true
 }));
 app.use(express.json());
